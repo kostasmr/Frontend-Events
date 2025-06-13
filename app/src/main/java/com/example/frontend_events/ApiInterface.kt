a456c35025a3075633bd40801dfa044d80f0d940
Konstantinos Mourousidis <kwstasmr7@gmail.com>
2025-06-13 15:23:11 +0300

    Add a new request

--- app/src/main/java/com/example/frontend_events/ApiInterface.kt
+++ app/src/main/java/com/example/frontend_events/ApiInterface.kt
@@ -2,10 +2,15 @@ package com.example.frontend_events
 
 import com.example.frontend_events.models.Event
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiInterface {
 
     @GET(value = "events")
     fun getData(): Call<List<Event>>
+
+    @POST(value = "recoms")
+    fun getRecomEvents(@Body body: List<String>): Call<List<Event>>
 }
