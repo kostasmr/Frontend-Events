2e2542b246d534baca4a0a27250dad6f50912b29
Konstantinos Mourousidis <kwstasmr7@gmail.com>
2025-06-16 17:44:26 +0300

    Update EventActivity.kt

--- app/src/main/java/com/example/frontend_events/activity/EventActivity.kt
+++ app/src/main/java/com/example/frontend_events/activity/EventActivity.kt
@@ -9,8 +9,20 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import com.bumptech.glide.Glide
 import com.example.frontend_events.R
 import com.example.frontend_events.models.Event
+import org.osmdroid.config.Configuration
+import org.osmdroid.views.MapView
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.Marker
+import android.net.Uri
+
 
 class EventActivity : AppCompatActivity() {
+
+    private lateinit var map: MapView
+    // Replace with your coordinates
+    val lat = 37.9838
+    val lng = 23.7275
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event)
@@ -61,5 +73,49 @@ class EventActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        //map content
+
+        Configuration.getInstance().load(applicationContext, getSharedPreferences("osmdroid", MODE_PRIVATE))
+        map = findViewById(R.id.map)
+        map.setMultiTouchControls(true) // enable zoom controls
+
+        val startPoint = GeoPoint(lat, lng)
+        map.controller.setZoom(16.0)
+        map.controller.setCenter(startPoint)
+
+        // Add a marker
+        val marker = Marker(map)
+        marker.position = startPoint
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        marker.title = "Athens"
+        map.overlays.add(marker)
+
+        val openMapsButton = findViewById<TextView>(R.id.locationBtn)
+        openMapsButton.setOnClickListener {
+            val gmmIntentUri = Uri.parse("geo:${lat},${lng}?q=${lat},${lng}(${event.schedule[0].location})")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps") // Optional: to prefer Google Maps app
+
+            // Check if an app exists to handle the intent
+            if (mapIntent.resolveActivity(packageManager) != null) {
+                startActivity(mapIntent)
+            } else {
+                // Fallback: open in browser or show message
+                val browserIntent = Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.google.com/maps/search/?api=1&query=${lat},${lng}"))
+                startActivity(browserIntent)
+            }
+        }
+
+        // chat
+
+        val chatBtn = findViewById<ImageView>(R.id.chatBtn)
+        buyBtn.setOnClickListener {
+            val intent = Intent(this@EventActivity, ChatActivity::class.java)
+            intent.putExtra("event", event)
+            startActivity(intent)
+        }
+
     }
+
 }
