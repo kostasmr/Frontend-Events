9b698aa8002a81a98923bf673993071bbbec95ab
Konstantinos Mourousidis <kwstasmr7@gmail.com>
2025-06-21 14:08:49 +0300

    Update AppState.kt

--- app/src/main/java/com/example/frontend_events/AppState.kt
+++ app/src/main/java/com/example/frontend_events/AppState.kt
@@ -1,5 +1,8 @@
 package com.example.frontend_events
 
+import com.example.frontend_events.models.User
+
 object AppState {
     var recomCategories: ArrayList<String> = arrayListOf()
+    var currentUser: User ?= null
 }
