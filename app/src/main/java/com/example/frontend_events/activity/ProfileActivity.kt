56d3a5f74475c2b79588dcf0d531d78e18b357ff
Konstantinos Mourousidis <kwstasmr7@gmail.com>
2025-06-21 17:07:06 +0300

    Update ProfileActivity.kt

--- app/src/main/java/com/example/frontend_events/activity/ProfileActivity.kt
+++ app/src/main/java/com/example/frontend_events/activity/ProfileActivity.kt
@@ -1,5 +1,6 @@
 package com.example.frontend_events.activity
 
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
@@ -22,10 +23,18 @@ class ProfileActivity : AppCompatActivity() {
 
         //menu bar
         val home = findViewById<ImageView>(R.id.homeBtn)
+        val ticketsBtn = findViewById<ImageView>(R.id.ticketsBtn)
+        val favoritesBtn = findViewById<ImageView>(R.id.favoritesBtn)
 
         home.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
+            finish()
+        }
+        ticketsBtn.setOnClickListener {
+            val intent = Intent(this, TicketListActivity::class.java)
+            startActivity(intent)
+            finish()
         }
 
         val editBtn = findViewById<AppCompatButton>(R.id.editBtn)
@@ -44,5 +53,44 @@ class ProfileActivity : AppCompatActivity() {
         }else{
             aboutText.setText(user.about)
         }
+
+
+        // Events gallery
+        val imageView1 = findViewById<ImageView>(R.id.imageView28)
+        val imageView2 = findViewById<ImageView>(R.id.imageView29)
+        val imageView3 = findViewById<ImageView>(R.id.imageView30)
+        val imageView4 = findViewById<ImageView>(R.id.imageView31)
+
+        imageView1.setOnClickListener {
+            showImagePopup(R.drawable.img_onboarding1)
+        }
+
+        imageView2.setOnClickListener {
+            showImagePopup(R.drawable.popular2)
+        }
+
+        imageView3.setOnClickListener {
+            showImagePopup(R.drawable.img_recom1)
+        }
+
+        imageView4.setOnClickListener {
+            showImagePopup(R.drawable.img_onboarding6)
+        }
     }
+
+    private fun showImagePopup(imageResId: Int) {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.events_gallery_popup)
+
+        val imageView = dialog.findViewById<ImageView>(R.id.popupImageView)
+        imageView.setImageResource(imageResId)
+
+        // Close dialog when user taps the image
+        imageView.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
 }
